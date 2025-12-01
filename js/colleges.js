1b8a82fc41d6327f689d5c8fb2ee1650ef4d1532
thanhthu2411 <thu24@byui.edu>
2025-12-01 12:10:44 -0700

    Add college search form handling
    Implement search functionality for colleges.

--- js/colleges.js
+++ js/colleges.js
@@ -1,1 +1,15 @@
+// handle search form
+document.querySelector(".college-search-form").addEventListener("submit", async function Submit (event) {
+    event.preventDefault();
+
+    const input = document.querySelector(".search-input").value;
+    const searchedList = await getSearchResults(input);
+
+    const societyList = document.querySelector(".colleges-container");
+    societyList.innerHTML = "";
+
+    for (const college of searchedList) {
+        societyList.innerHTML += collegeTemplate(college);
+    }
+})
 
